7904943fb78c6b55d57ad567eae5311fb4b161ca
nesetbk <72620201+nesetbk@users.noreply.github.com>
2020-12-15 14:31:45 +0300

    güncel

--- yazilimEgitim/resources/views/front/blog.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.front')
-@section('title','Blog Page')
-
-@section('sections')
-    @include('front.section.blogsection.blog-page-section')
-@endsection
-
-
--- yazilimEgitim/resources/views/front/section/aboutsection/bannersection.blade.php
+++
@@ -1,6 +0,0 @@
-<section class="banner-page-section">
-    <div class="container">
-        <h1>About Us</h1>
-        <p>Tell the world about yourself</p>
-    </div>
-</section>
